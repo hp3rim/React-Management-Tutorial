bbcdd63469828848e2b299ff5fe534d27555c266
이동준 <nal-en@idongjun-ui-MacBook-Pro.local>
2019-06-24 15:18:31 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/any/1',
+    'name' : '이동준',
+    'birthday' : '891225',
+    'gender' : '남자',
+    'job' : '웹개발자',
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/any/2',
+    'name' : '홍길동',
+    'birthday' : '190123',
+    'gender' : '남자',
+    'job' : '의적',
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/any/3',
+    'name' : '하니',
+    'birthday' : '911015',
+    'gender' : '여자',
+    'job' : '마라토너',
+  },
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return ( 
+              <Customer 
+                key={c.id}
+                id={c.id}
+                name={c.name}
+                image={c.image}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />          
+            );
+          })
+        }
+      </div>      
+    );
+  }
 }
 
 export default App;
